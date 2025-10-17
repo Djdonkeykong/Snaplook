44603f0a7ad7dd7d27fc33cd7c6727eb86844bad
Djdonkeykong <ole_4390@hotmail.com>
2025-10-17 20:13:16 +0200

    fix

--- ios/shareExtension/WebViewController.swift
+++ ios/shareExtension/WebViewController.swift
@@ -17,17 +17,10 @@ class WebViewController: UIViewController, WKNavigationDelegate {
 
     // Browser UI elements
     private var toolbarContainer: UIView!
-    private var backButton: UIButton!
-    private var forwardButton: UIButton!
     private var urlBar: UIView!
     private var urlLabel: UILabel!
     private var lockIcon: UIImageView!
-    private var refreshButton: UIButton!
-    private var shareButton: UIButton!
-
-    // Shimmer loading
-    private var shimmerView: UIView!
-    private var shimmerGradientLayer: CAGradientLayer!
+    private var doneButton: UIButton!
 
     init(url: URL, shareViewController: RSIShareViewController) {
         self.url = url
@@ -46,8 +39,6 @@ class WebViewController: UIViewController, WKNavigationDelegate {
 
         setupToolbar()
         setupWebView()
-        setupShimmer()
-        setupNavigationBar()
 
         // Load the URL
         let request = URLRequest(url: url)
@@ -56,38 +47,11 @@ class WebViewController: UIViewController, WKNavigationDelegate {
         NSLog("[ShareExtension] WebViewController loading URL: \(url.absoluteString)")
     }
 
-    private func setupNavigationBar() {
-        // Add Done button to navigation bar
-        let doneButton = UIBarButtonItem(
-            title: "Done",
-            style: .done,
-            target: self,
-            action: #selector(doneTapped)
-        )
-        navigationItem.rightBarButtonItem = doneButton
-    }
-
     private func setupToolbar() {
         // Container for toolbar
         toolbarContainer = UIView()
         toolbarContainer.backgroundColor = .systemBackground
         toolbarContainer.translatesAutoresizingMaskIntoConstraints = false
-
-        // Back button
-        backButton = UIButton(type: .system)
-        backButton.setImage(UIImage(systemName: "chevron.left"), for: .normal)
-        backButton.tintColor = .label
-        backButton.addTarget(self, action: #selector(backTapped), for: .touchUpInside)
-        backButton.translatesAutoresizingMaskIntoConstraints = false
-        backButton.isEnabled = false
-
-        // Forward button
-        forwardButton = UIButton(type: .system)
-        forwardButton.setImage(UIImage(systemName: "chevron.right"), for: .normal)
-        forwardButton.tintColor = .label
-        forwardButton.addTarget(self, action: #selector(forwardTapped), for: .touchUpInside)
-        forwardButton.translatesAutoresizingMaskIntoConstraints = false
-        forwardButton.isEnabled = false
 
         // URL bar background
         urlBar = UIView()
@@ -109,29 +73,20 @@ class WebViewController: UIViewController, WKNavigationDelegate {
         urlLabel.translatesAutoresizingMaskIntoConstraints = false
         urlLabel.text = url.host ?? url.absoluteString
 
-        // Refresh button
-        refreshButton = UIButton(type: .system)
-        refreshButton.setImage(UIImage(systemName: "arrow.clockwise"), for: .normal)
-        refreshButton.tintColor = .label
-        refreshButton.addTarget(self, action: #selector(refreshTapped), for: .touchUpInside)
-        refreshButton.translatesAutoresizingMaskIntoConstraints = false
-
-        // Share button
-        shareButton = UIButton(type: .system)
-        shareButton.setImage(UIImage(systemName: "square.and.arrow.up"), for: .normal)
-        shareButton.tintColor = .label
-        shareButton.addTarget(self, action: #selector(shareTapped), for: .touchUpInside)
-        shareButton.translatesAutoresizingMaskIntoConstraints = false
+        // Done button
+        doneButton = UIButton(type: .system)
+        doneButton.setTitle("Done", for: .normal)
+        doneButton.titleLabel?.font = .systemFont(ofSize: 16, weight: .medium)
+        doneButton.tintColor = .label
+        doneButton.addTarget(self, action: #selector(doneTapped), for: .touchUpInside)
+        doneButton.translatesAutoresizingMaskIntoConstraints = false
 
         // Add to toolbar
         urlBar.addSubview(lockIcon)
         urlBar.addSubview(urlLabel)
 
-        toolbarContainer.addSubview(backButton)
-        toolbarContainer.addSubview(forwardButton)
         toolbarContainer.addSubview(urlBar)
-        toolbarContainer.addSubview(refreshButton)
-        toolbarContainer.addSubview(shareButton)
+        toolbarContainer.addSubview(doneButton)
 
         view.addSubview(toolbarContainer)
 
@@ -148,33 +103,9 @@ class WebViewController: UIViewController, WKNavigationDelegate {
             toolbarContainer.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             toolbarContainer.heightAnchor.constraint(equalToConstant: 52),
 
-            // Back button
-            backButton.leadingAnchor.constraint(equalTo: toolbarContainer.leadingAnchor, constant: 12),
-            backButton.centerYAnchor.constraint(equalTo: toolbarContainer.centerYAnchor),
-            backButton.widthAnchor.constraint(equalToConstant: 32),
-            backButton.heightAnchor.constraint(equalToConstant: 32),
-
-            // Forward button
-            forwardButton.leadingAnchor.constraint(equalTo: backButton.trailingAnchor, constant: 4),
-            forwardButton.centerYAnchor.constraint(equalTo: toolbarContainer.centerYAnchor),
-            forwardButton.widthAnchor.constraint(equalToConstant: 32),
-            forwardButton.heightAnchor.constraint(equalToConstant: 32),
-
-            // Share button (positioned before refresh)
-            shareButton.trailingAnchor.constraint(equalTo: toolbarContainer.trailingAnchor, constant: -12),
-            shareButton.centerYAnchor.constraint(equalTo: toolbarContainer.centerYAnchor),
-            shareButton.widthAnchor.constraint(equalToConstant: 32),
-            shareButton.heightAnchor.constraint(equalToConstant: 32),
-
-            // Refresh button
-            refreshButton.trailingAnchor.constraint(equalTo: shareButton.leadingAnchor, constant: -4),
-            refreshButton.centerYAnchor.constraint(equalTo: toolbarContainer.centerYAnchor),
-            refreshButton.widthAnchor.constraint(equalToConstant: 32),
-            refreshButton.heightAnchor.constraint(equalToConstant: 32),
-
-            // URL bar
-            urlBar.leadingAnchor.constraint(equalTo: forwardButton.trailingAnchor, constant: 8),
-            urlBar.trailingAnchor.constraint(equalTo: refreshButton.leadingAnchor, constant: -8),
+            // URL bar - extends further right now that arrows are removed
+            urlBar.leadingAnchor.constraint(equalTo: toolbarContainer.leadingAnchor, constant: 16),
+            urlBar.trailingAnchor.constraint(equalTo: doneButton.leadingAnchor, constant: -12),
             urlBar.centerYAnchor.constraint(equalTo: toolbarContainer.centerYAnchor),
             urlBar.heightAnchor.constraint(equalToConstant: 36),
 
@@ -189,6 +120,11 @@ class WebViewController: UIViewController, WKNavigationDelegate {
             urlLabel.trailingAnchor.constraint(equalTo: urlBar.trailingAnchor, constant: -10),
             urlLabel.centerYAnchor.constraint(equalTo: urlBar.centerYAnchor),
 
+            // Done button (positioned where share/refresh were)
+            doneButton.trailingAnchor.constraint(equalTo: toolbarContainer.trailingAnchor, constant: -16),
+            doneButton.centerYAnchor.constraint(equalTo: toolbarContainer.centerYAnchor),
+            doneButton.widthAnchor.constraint(greaterThanOrEqualToConstant: 50),
+
             // Separator
             separator.leadingAnchor.constraint(equalTo: toolbarContainer.leadingAnchor),
             separator.trailingAnchor.constraint(equalTo: toolbarContainer.trailingAnchor),
@@ -198,11 +134,11 @@ class WebViewController: UIViewController, WKNavigationDelegate {
     }
 
     private func setupWebView() {
-        // Progress bar for loading indicator
+        // Progress bar for loading indicator (underneath toolbar)
         progressView = UIProgressView(progressViewStyle: .default)
         progressView.translatesAutoresizingMaskIntoConstraints = false
         progressView.progressTintColor = UIColor(red: 242/255, green: 0, blue: 60/255, alpha: 1.0)
-        progressView.trackTintColor = .clear
+        progressView.trackTintColor = .systemGray6
         progressView.isHidden = true
 
         // Configure web view
@@ -214,111 +150,27 @@ class WebViewController: UIViewController, WKNavigationDelegate {
         webView.navigationDelegate = self
         webView.translatesAutoresizingMaskIntoConstraints = false
         webView.allowsBackForwardNavigationGestures = true
-        webView.alpha = 0 // Start hidden for shimmer effect
 
-        view.addSubview(webView)
         view.addSubview(progressView)
+        view.addSubview(webView)
 
         NSLayoutConstraint.activate([
+            // Progress bar directly under toolbar
             progressView.topAnchor.constraint(equalTo: toolbarContainer.bottomAnchor),
             progressView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             progressView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             progressView.heightAnchor.constraint(equalToConstant: 2),
 
+            // Web view below progress bar
             webView.topAnchor.constraint(equalTo: progressView.bottomAnchor),
             webView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             webView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             webView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
 
-        // Observe loading progress
+        // Observe loading progress and URL
         webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
-        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.canGoBack), options: .new, context: nil)
-        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.canGoForward), options: .new, context: nil)
         webView.addObserver(self, forKeyPath: #keyPath(WKWebView.url), options: .new, context: nil)
-    }
-
-    private func setupShimmer() {
-        // Shimmer container
-        shimmerView = UIView()
-        shimmerView.backgroundColor = .systemBackground
-        shimmerView.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(shimmerView)
-
-        NSLayoutConstraint.activate([
-            shimmerView.topAnchor.constraint(equalTo: toolbarContainer.bottomAnchor),
-            shimmerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            shimmerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            shimmerView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
-        ])
-
-        // Create shimmer boxes to simulate content loading
-        let shimmerBoxes = createShimmerBoxes()
-        shimmerBoxes.forEach { shimmerView.addSubview($0) }
-
-        // Create shimmer gradient
-        shimmerGradientLayer = CAGradientLayer()
-        shimmerGradientLayer.colors = [
-            UIColor.systemGray6.cgColor,
-            UIColor.systemGray5.cgColor,
-            UIColor.systemGray6.cgColor
-        ]
-        shimmerGradientLayer.startPoint = CGPoint(x: 0, y: 0.5)
-        shimmerGradientLayer.endPoint = CGPoint(x: 1, y: 0.5)
-        shimmerGradientLayer.locations = [0, 0.5, 1]
-        shimmerView.layer.addSublayer(shimmerGradientLayer)
-
-        startShimmerAnimation()
-    }
-
-    private func createShimmerBoxes() -> [UIView] {
-        var boxes: [UIView] = []
-
-        // Create various sized boxes to simulate content
-        let boxConfigs: [(CGFloat, CGFloat, CGFloat, CGFloat)] = [
-            // (x, y, width, height)
-            (16, 20, 200, 20),      // Title
-            (16, 50, 150, 16),      // Subtitle
-            (16, 80, view.bounds.width - 32, 200), // Image placeholder
-            (16, 300, view.bounds.width - 32, 16), // Line 1
-            (16, 330, view.bounds.width - 80, 16), // Line 2
-            (16, 360, view.bounds.width - 50, 16), // Line 3
-            (16, 400, 120, 40),     // Button
-        ]
-
-        for config in boxConfigs {
-            let box = UIView()
-            box.backgroundColor = .systemGray6
-            box.layer.cornerRadius = 8
-            box.frame = CGRect(x: config.0, y: config.1, width: config.2, height: config.3)
-            boxes.append(box)
-        }
-
-        return boxes
-    }
-
-    private func startShimmerAnimation() {
-        let animation = CABasicAnimation(keyPath: "locations")
-        animation.fromValue = [-1.0, -0.5, 0.0]
-        animation.toValue = [1.0, 1.5, 2.0]
-        animation.duration = 1.5
-        animation.repeatCount = .infinity
-        shimmerGradientLayer?.add(animation, forKey: "shimmer")
-    }
-
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        shimmerGradientLayer?.frame = shimmerView.bounds
-    }
-
-    private func hideShimmer() {
-        UIView.animate(withDuration: 0.3, animations: {
-            self.shimmerView.alpha = 0
-            self.webView.alpha = 1
-        }) { _ in
-            self.shimmerView.removeFromSuperview()
-            self.shimmerGradientLayer?.removeFromSuperlayer()
-        }
     }
 
     override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
@@ -327,6 +179,7 @@ class WebViewController: UIViewController, WKNavigationDelegate {
             progressView.setProgress(progress, animated: true)
 
             if progress >= 1.0 {
+                // Fade out progress bar when loading completes
                 UIView.animate(withDuration: 0.3, delay: 0.3, options: .curveEaseOut, animations: {
                     self.progressView.alpha = 0
                 }, completion: { _ in
@@ -335,13 +188,8 @@ class WebViewController: UIViewController, WKNavigationDelegate {
                 })
             } else {
                 progressView.isHidden = false
+                progressView.alpha = 1
             }
-        } else if keyPath == "canGoBack" {
-            backButton.isEnabled = webView.canGoBack
-            backButton.alpha = webView.canGoBack ? 1.0 : 0.3
-        } else if keyPath == "canGoForward" {
-            forwardButton.isEnabled = webView.canGoForward
-            forwardButton.alpha = webView.canGoForward ? 1.0 : 0.3
         } else if keyPath == "url" {
             if let url = webView.url {
                 urlLabel.text = url.host ?? url.absoluteString
@@ -351,25 +199,6 @@ class WebViewController: UIViewController, WKNavigationDelegate {
     }
 
     // MARK: - Actions
-
-    @objc private func backTapped() {
-        webView.goBack()
-    }
-
-    @objc private func forwardTapped() {
-        webView.goForward()
-    }
-
-    @objc private func refreshTapped() {
-        webView.reload()
-    }
-
-    @objc private func shareTapped() {
-        guard let url = webView.url else { return }
-        let activityVC = UIActivityViewController(activityItems: [url], applicationActivities: nil)
-        activityVC.popoverPresentationController?.sourceView = shareButton
-        present(activityVC, animated: true)
-    }
 
     @objc private func doneTapped() {
         NSLog("[ShareExtension] Done button tapped in WebViewController - dismissing web view")
@@ -388,23 +217,16 @@ class WebViewController: UIViewController, WKNavigationDelegate {
     }
 
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-        navigationItem.title = webView.title
-
-        // Hide shimmer when content loads
-        if shimmerView.superview != nil {
-            hideShimmer()
-        }
+        NSLog("[ShareExtension] WebView finished loading: \(webView.url?.absoluteString ?? "unknown")")
     }
 
     func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
         NSLog("[ShareExtension] WebView failed to load: \(error.localizedDescription)")
-        hideShimmer()
         showError(message: "Failed to load page")
     }
 
     func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
         NSLog("[ShareExtension] WebView failed provisional navigation: \(error.localizedDescription)")
-        hideShimmer()
         showError(message: "Failed to load page")
     }
 
@@ -416,8 +238,6 @@ class WebViewController: UIViewController, WKNavigationDelegate {
 
     deinit {
         webView?.removeObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress))
-        webView?.removeObserver(self, forKeyPath: #keyPath(WKWebView.canGoBack))
-        webView?.removeObserver(self, forKeyPath: #keyPath(WKWebView.canGoForward))
         webView?.removeObserver(self, forKeyPath: #keyPath(WKWebView.url))
     }
 }
