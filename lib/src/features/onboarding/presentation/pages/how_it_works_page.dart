faec906982f3adc8743a19b342cfc218e6724e5a
Djdonkeykong <ole_4390@hotmail.com>
2025-12-16 15:23:50 +0100

    fix

--- lib/src/features/onboarding/presentation/pages/how_it_works_page.dart
+++ lib/src/features/onboarding/presentation/pages/how_it_works_page.dart
@@ -183,7 +183,7 @@ class _HowItWorksPageState extends State<HowItWorksPage> {
           Positioned(
             left: spacing.l,
             right: spacing.l,
-            bottom: 32,
+            bottom: 46,
             child: SizedBox(
               height: buttonHeight,
               child: ElevatedButton(
