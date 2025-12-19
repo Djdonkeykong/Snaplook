ebbe84e005b97b62e6ed60e842b4e6f3ec61da6f
Djdonkeykong <ole_4390@hotmail.com>
2025-12-19 17:10:48 +0100

    fix

--- lib/src/features/auth/presentation/pages/email_verification_page.dart
+++ lib/src/features/auth/presentation/pages/email_verification_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:purchases_flutter/purchases_flutter.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import '../../../../../core/theme/app_colors.dart';
 import '../../../../../core/theme/theme_extensions.dart';
--- lib/src/features/auth/presentation/pages/login_page.dart
+++ lib/src/features/auth/presentation/pages/login_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:purchases_flutter/purchases_flutter.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:video_player/video_player.dart';
 import 'package:url_launcher/url_launcher.dart';
